c1ab5e5ce5b139a138da38bccdcb6ec47c20d893
Chris Grieger <chris@scolp.de>
2016-02-15 10:58:24 +0100

    optimization: remove duplicate code

--- lib/worker.ex
+++ lib/worker.ex
@@ -19,12 +19,12 @@ defmodule Metrics.Worker do
   ###
   @doc "Set gauge to value n."
   def set_gauge(name, n) do
-    GenServer.cast Metrics, {:set_gauge, name, n}
+    GenServer.cast Metrics, {:set, [:gauges, name], n}
   end
 
   @doc "Get gauge value."
   def get_gauge(name) do
-    GenServer.call Metrics, {:get_gauge, name}
+    GenServer.call Metrics, {:get, [:gauges, name]}
   end
 
   @doc "Remove gauge from collection."
@@ -42,33 +42,24 @@ defmodule Metrics.Worker do
 
   @doc "Returns the counter value with 'name'. Returns 0 if this counter is unknown."
   def get_counter(name) do
-    GenServer.call Metrics, {:get_counter, name}
+    GenServer.call Metrics, {:get, [:counters, name]}
   end
 
   @doc "Reset counter."
   def reset_counter(name, n \\ 0) do
-    GenServer.cast Metrics, {:set_counter, name, n}
+    GenServer.cast Metrics, {:set, [:counters, name], n}
   end
 
   ###
   # Server Functions
   ###
 
-  def handle_call({:get_counter, counter_name}, _from, state) do
-    {:reply, get_in(state, [:counters, counter_name]), state}
-  end
-
-  def handle_call({:get_gauge, gauge_name}, _from, state) do
-    {:reply, get_in(state, [:gauges, gauge_name]), state}
+  def handle_call({:get, path}, _from, state) do
+    {:reply, get_in(state, path), state}
   end
 
   def handle_call(:get_state, _from, state) do
     {:reply, state, state}
-  end
-
-  def handle_cast({:set_counter, counter_name, n}, state) do
-    state = update_in(state, [:counters, counter_name], fn _ -> n end)
-    {:noreply, state}
   end
 
   def handle_cast({:increment_counter, counter_name, n}, state) do
@@ -76,8 +67,8 @@ defmodule Metrics.Worker do
     {:noreply, state}
   end
 
-  def handle_cast({:set_gauge, gauge_name, n}, state) do
-    state = update_in(state, [:gauges, gauge_name], fn _ -> n end)
+  def handle_cast({:set, path, value}, state) do
+    state = update_in(state, path, fn _ -> value end)
     {:noreply, state}
   end
 
